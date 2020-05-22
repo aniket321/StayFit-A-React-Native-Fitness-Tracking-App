9c69a6cebdf70960a1d7d5f58aae2ebf666c56f5
aniket.shashikant <aniket.shashikant@hashedin.com>
2020-05-22 21:24:40 +0530

    feat: added increment, decrement and slide methods to update the state in AddEntry file

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './Components/AddEntry'
 
-const instructions = Platform.select({
-    ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-    android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
-
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.welcome}> bohot khushi hui Chal gaya nasdf bhai</Text>
-            <Text style={styles.instructions}>Ttarted, edit App.js</Text>
-            <Text style={styles.instructions}>{instructions}</Text>
-        </View>
-    );
+export default class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <AddEntry />
+            </View>
+        )
+    }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
