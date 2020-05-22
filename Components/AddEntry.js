3714e42d863fcadb0a2d3a601d1a83c16a2ebe2e
aniket.shashikant <aniket.shashikant@hashedin.com>
2020-05-22 22:26:39 +0530

    feat: added functionality to check if the user has already logged the information for a particular day

--- Components/AddEntry.js
+++ Components/AddEntry.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
-import Sliders from './Sliders'
-import Stepper from './Stepper'
-import DateHeader from './DateHeader'
+import React, { Component } from "react";
+import { View, TouchableOpacity, Text } from "react-native";
+import { getMetricMetaInfo, timeToString } from "../utils/helpers";
+import Sliders from "./Sliders";
+import Stepper from "./Stepper";
+import DateHeader from "./DateHeader";
+import { Ionicons } from "@expo/vector-icons";
+import TextButton from "./TextButton";
 
 function SubmitBtn({ onPress }) {
     return (
-        <TouchableOpacity
-            onPress={onPress}>
+        <TouchableOpacity onPress={onPress}>
             <Text>SUBMIT</Text>
         </TouchableOpacity>
-    )
+    );
 }
 
 export default class AddEntry extends Component {
@@ -20,72 +21,87 @@ export default class AddEntry extends Component {
         bike: 0,
         swim: 0,
         sleep: 0,
-        eat: 0,
-    }
-    increment = (metric) => {
-        const { max, step } = getMetricMetaInfo(metric)
+        eat: 0
+    };
+    increment = metric => {
+        const { max, step } = getMetricMetaInfo(metric);
 
-        this.setState((state) => {
-            const count = state[metric] + step
-
-            return {
-                ...state,
-                [metric]: count > max ? max : count,
-            }
-        })
-    }
-    decrement = (metric) => {
-        this.setState((state) => {
-            const count = state[metric] - getMetricMetaInfo(metric).step
+        this.setState(state => {
+            const count = state[metric] + step;
 
             return {
                 ...state,
-                [metric]: count < 0 ? 0 : count,
-            }
-        })
-    }
+                [metric]: count > max ? max : count
+            };
+        });
+    };
+    decrement = metric => {
+        this.setState(state => {
+            const count = state[metric] - getMetricMetaInfo(metric).step;
+
+            return {
+                ...state,
+                [metric]: count < 0 ? 0 : count
+            };
+        });
+    };
     slide = (metric, value) => {
         this.setState(() => ({
             [metric]: value
-        }))
-    }
+        }));
+    };
     submit = () => {
-        const key = timeToString()
-        const entry = this.state
+        const key = timeToString();
+        const entry = this.state;
 
-        this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
-    }
+        this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }));
+
+    };
+    reset = () => {
+        const key = timeToString();
+    };
     render() {
-        const metaInfo = getMetricMetaInfo()
+        const metaInfo = getMetricMetaInfo();
+
+        if (this.props.alreadyLogged) {
+            return (
+                <View>
+                    <Ionicons name={"ios-happy"} size={100} />
+                    <Text>You already logged your information for today.</Text>
+                    <TextButton onPress={this.reset}>Reset</TextButton>
+                </View>
+            );
+        }
 
         return (
             <View>
-                <Text>{JSON.stringify(this.state)}</Text>
-                <DateHeader date={(new Date()).toLocaleDateString()} />
-                {Object.keys(metaInfo).map((key) => {
-                    const { getIcon, type, ...rest } = metaInfo[key]
-                    const value = this.state[key]
+                <DateHeader date={new Date().toLocaleDateString()} />
+                {Object.keys(metaInfo).map(key => {
+                    const { getIcon, type, ...rest } = metaInfo[key];
+                    const value = this.state[key];
 
                     return (
                         <View key={key}>
                             {getIcon()}
-                            {type === 'slider'
-                                ? <Sliders
+                            {type === "slider" ? (
+                                <Sliders
                                     value={value}
-                                    onChange={(value) => this.slide(key, value)}
+                                    onChange={value => this.slide(key, value)}
                                     {...rest}
                                 />
-                                : <Stepper
-                                    value={value}
-                                    onIncrement={() => this.increment(key)}
-                                    onDecrement={() => this.decrement(key)}
-                                    {...rest}
-                                />}
+                            ) : (
+                                    <Stepper
+                                        value={value}
+                                        onIncrement={() => this.increment(key)}
+                                        onDecrement={() => this.decrement(key)}
+                                        {...rest}
+                                    />
+                                )}
                         </View>
-                    )
+                    );
                 })}
                 <SubmitBtn onPress={this.submit} />
             </View>
-        )
+        );
     }
 }
